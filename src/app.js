c516cb3a2ef44a524bf2a76efd66ec1dd9aa0cba
hsinlive <hsinlive@gmail.com>
2018-08-07 16:11:40 +0800

    feat(global): Set-up react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log('hello World')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+text-align: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
