8ec89536f81f05a8cbb3a1433bf9ea7c5c9c9827
hsinlive <hsinlive@gmail.com>
2018-08-08 14:17:32 +0800

    fix(global): node not exit whtn electron app close

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
+    process.exit()
   })
 }
 
